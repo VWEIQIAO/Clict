c125c48b134f41bdbe885204ee6a0cf922b6d387
ASUS <ASUS@ASUS-PC>
2017-04-01 10:14:42 +0800

    abc

---
+++ src/com/VWEIQIAO/Clict/App.java
@@ -0,0 +1,11 @@
+package com.VWEIQIAO.Clict;
+/**
+ * 
+ * @author ASUS
+ *
+ */
+public class App {
+public static void main (String[]args){
+	AppJFrame frame=new AppJFrame();
+}
+}
---
+++ src/com/VWEIQIAO/Clict/AppJFrame.java
@@ -0,0 +1,21 @@
+package com.VWEIQIAO.Clict;
+
+import javax.swing.JFrame;
+/**
+ * 
+ * @author ASUS
+ *
+ */
+public class AppJFrame extends JFrame {
+/** 
+ * 构造方法
+ */
+	public AppJFrame(){
+		setTitle("五子棋");
+		setSize(500,500);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
